1c30ae950a18d060383c6330e9c540eacc75eecf
sinanekmekcibasi <sinanekmekcibasi@gmail.com>
2022-07-01 16:54:35 +0300

    edited main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,6 @@
 // g++ metis.cc -lmetis
 int main(){
     
-    idx_t nVertices = 6;
     idx_t nEdges    = 7;
     idx_t nWeights  = 1;
     idx_t nParts    = 4;
@@ -43,48 +42,24 @@ int main(){
         J[i]--;
     }
     file.close();
-    /* 
-    for(int i = 0; i < 100; i++)
-    {
-        std::cout<<I[i]<< " "<< J[i] <<" "<<val[i]<<std::endl;
-    }
-    */
-      
+     
     csr_col = (idx_t *)malloc(numOfVal * sizeof(idx_t));
     csr_val = (double *)malloc(numOfVal * sizeof(double));
     csr_row = (idx_t *)malloc((numOfRow + 2) * sizeof(idx_t));
     
     for(idx_t i = 0; i < numOfVal; i++){
         csr_row[I[i] + 1]++;
-        //csr_val[i] = val[i];
-        //csr_col[i] = J[i];
-    }
+   }
     for(idx_t i = 0; i < numOfRow; i++){
         csr_row[i+1] += csr_row[i];
     }
-    //for(idx_t i = 0; i < numOfRow; i++){
-    //    std::cout<<csr_row[i]<<" ";
-    //}
     std::cout<<std::endl;
     
     for(idx_t i = 0; i < numOfVal; i++){
         csr_col[csr_row[I[i] + 1]] = J[i];
         csr_val[csr_row[I[i]+1]++] = val[i];
     }
-    
-    // Indexes of starting points in adjacent array
-
-    // Weights of vertices
-    // if all weights are equal then can be set to NULL
-    //idx_t vwgt[nVertices * nWeights];
-    
-   /* 
-     int ret = METIS_PartGraphRecursive(&nVertices,& nWeights, xadj, adjncy,
-     				       NULL, NULL, NULL, &nParts, NULL,
-     				       NULL, NULL, &objval, part);
-    */
-    std::cout<<numOfRow<<std::endl;
-    //numOfRow+=10;
+        
     idx_t options[METIS_NOPTIONS];
     options[METIS_OPTION_OBJTYPE] = METIS_OBJTYPE_CUT;
     idx_t asfas = 6;
@@ -95,7 +70,7 @@ int main(){
     std::cout << ret << std::endl;
     
     for(unsigned part_i = 0; part_i < numOfRow; part_i++){
-	std::cout << part_i << " " << part[part_i] << std::endl;
+	    std::cout << part_i << " " << part[part_i] << std::endl;
     }
     
    
